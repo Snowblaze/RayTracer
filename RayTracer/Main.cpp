513ed0c4a6b8cae6af84f34f991ad35af964e2b0
Snowblaze <a.a.matinyan@gmail.com>
2020-02-20 23:31:33 +0400

    Add vec3 and update main

--- RayTracer/Main.cpp
+++ RayTracer/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include "vec3.h"
 
 using namespace std;
 
@@ -17,12 +18,10 @@ int main()
         {
             for (int i = 0; i < nx; i++)
             {
-                float r = float(i) / float(nx);
-                float g = float(j) / float(ny);
-                float b = 0.2f;
-                int ir = int(255.99 * r);
-                int ig = int(255.99 * g);
-                int ib = int(255.99 * b);
+                vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+                int ir = int(255.99 * col.r);
+                int ig = int(255.99 * col.g);
+                int ib = int(255.99 * col.b);
                 file << ir << " " << ig << " " << ib << "\n";
             }
         }
