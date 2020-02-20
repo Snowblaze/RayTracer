1c9255deca50515970a850ea5f171eb2c00f0c10
Snowblaze <a.a.matinyan@gmail.com>
2020-02-21 00:05:40 +0400

    Fix compilation error

--- RayTracer/Main.cpp
+++ RayTracer/Main.cpp
@@ -19,9 +19,9 @@ int main()
             for (int i = 0; i < nx; i++)
             {
                 vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
-                int ir = int(255.99 * col.r);
-                int ig = int(255.99 * col.g);
-                int ib = int(255.99 * col.b);
+                int ir = int(255.99 * col[0]);
+                int ig = int(255.99 * col[1]);
+                int ib = int(255.99 * col[2]);
                 file << ir << " " << ig << " " << ib << "\n";
             }
         }
