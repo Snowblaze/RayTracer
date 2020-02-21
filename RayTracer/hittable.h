46ba9bf94bdf7921dff520ab17be46951d452c7b
Snowblaze <a.a.matinyan@gmail.com>
2020-02-22 02:11:25 +0400

    Add hittable class and improve main

---
+++ RayTracer/hittable.h
@@ -0,0 +1,20 @@
+#ifndef HITTABLEH
+#define HITTABLEH
+#pragma once
+
+#include "ray.h"
+
+struct hit_record
+{
+    float t;
+    vec3 p;
+    vec3 normal;
+};
+
+class hittable
+{
+public:
+    virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
+};
+
+#endif
