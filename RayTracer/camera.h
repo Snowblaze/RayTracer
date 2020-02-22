21ff8b75add736bec7c8bf4ee2dab4582c6d7e59
Snowblaze <a.a.matinyan@gmail.com>
2020-02-23 01:51:05 +0400

    Make camera adjustable

--- RayTracer/camera.h
+++ RayTracer/camera.h
@@ -3,20 +3,28 @@
 #pragma once
 
 #include "ray.h"
+#include <corecrt_math_defines.h>
 
 class camera
 {
 public:
-    camera()
+    camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect)
     {
-        lower_left_corner = vec3(-2.0, -1.0, -1.0);
-        horizontal = vec3(4.0, 0.0, 0.0);
-        vertical = vec3(0.0, 2.0, 0.0);
-        origin = vec3(0.0, 0.0, 0.0);
+        vec3 u, v, w;
+        float theta = vfov * M_PI / 180.0;
+        float half_height = tan(theta / 2.0);
+        float half_width = aspect * half_height;
+        origin = lookfrom;
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
+        lower_left_corner = origin - half_width * u - half_height * v - w;
+        horizontal = 2.0 * half_width * u;
+        vertical = 2.0 * half_height * v;
     }
     ray get_ray(float u, float v)
     {
-        return ray(origin, lower_left_corner + u * horizontal + v * vertical);
+        return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
     }
 
     vec3 origin;
