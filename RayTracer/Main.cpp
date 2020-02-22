f9379e3072238191f5ebc584927525d42c34f455
Snowblaze <a.a.matinyan@gmail.com>
2020-02-22 11:18:27 +0400

    Add antialiasing

--- RayTracer/Main.cpp
+++ RayTracer/Main.cpp
@@ -4,6 +4,8 @@
 #include "hittable_list.h"
 #include "sphere.h"
 #include <float.h>
+#include "camera.h"
+#include "random.h"
 
 using namespace std;
 
@@ -31,29 +33,33 @@ int main()
     {
         int nx = 200;
         int ny = 100;
+        int ns = 100;
         file << "P3\n" << nx << " " << ny << "\n255\n";
-        vec3 lower_left_corner(-2.0, -1.0, -1.0);
-        vec3 horizontal(4.0, 0.0, 0.0);
-        vec3 vertical(0.0, 2.0, 0.0);
-        vec3 origin(0.0, 0.0, 0.0);
 
         hittable* list[2];
         list[0] = new sphere(vec3(0, 0, -1), 0.5);
         list[1] = new sphere(vec3(0, -100.5, -1), 100);
         hittable* world = new hittable_list(list, 2);
+        camera cam;
 
         for (int j = ny - 1; j >= 0; j--)
         {
             for (int i = 0; i < nx; i++)
             {
-                float u = float(i)/float(nx);
-                float v = float(j)/float(ny);
-                ray r(origin, lower_left_corner + horizontal * u + vertical * v);
-                vec3 p = r.point_at_parameter(2.0);
-                vec3 col = color(r, world);
+                vec3 col(0, 0, 0);
+                for (int s = 0; s < ns; s++)
+                {
+                    float u = float(i + random_double()) / float(nx);
+                    float v = float(j + random_double()) / float(ny);
+                    ray r = cam.get_ray(u, v);
+                    col += color(r, world);
+                }
+                col /= float(ns);
+
                 int ir = int(255.99 * col[0]);
                 int ig = int(255.99 * col[1]);
                 int ib = int(255.99 * col[2]);
+
                 file << ir << " " << ig << " " << ib << "\n";
             }
         }
