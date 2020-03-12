9abbe7bcc73669173a2c7a5e5d9c3d78d2a2198d
Snowblaze <a.a.matinyan@gmail.com>
2020-03-12 18:56:39 +0400

    Add final scene

--- RayTracer/bvh.h
+++ RayTracer/bvh.h
@@ -17,48 +17,6 @@ public:
     aabb box;
 };
 
-bvh_node::bvh_node(hitable** l, int n, float time0, float time1)
-{
-    int axis = int(3 * random_double());
-
-    if (axis == 0)
-    {
-        qsort(l, n, sizeof(hitable*), box_x_compare);
-    }
-    else if (axis == 1)
-    {
-        qsort(l, n, sizeof(hitable*), box_y_compare);
-    }
-    else
-    {
-        qsort(l, n, sizeof(hitable*), box_z_compare);
-    }
-
-    if (n == 1)
-    {
-        left = right = l[0];
-    }
-    else if (n == 2)
-    {
-        left = l[0];
-        right = l[1];
-    }
-    else
-    {
-        left = new bvh_node(l, n / 2, time0, time1);
-        right = new bvh_node(l + n / 2, n - n / 2, time0, time1);
-    }
-
-    aabb box_left;
-    aabb box_right;
-    if (!left->bounding_box(time0, time1, box_left) || !right->bounding_box(time0, time1, box_right))
-    {
-        std::cerr << "no bounding box in bvh_node constructor\n";
-    }
-
-    box = surrounding_box(box_left, box_right);
-}
-
 bool bvh_node::bounding_box(float t0, float t1, aabb& b) const
 {
     b = box;
@@ -165,4 +123,47 @@ int box_z_compare(const void* a, const void* b)
         return 1;
     }
 }
+
+bvh_node::bvh_node(hitable** l, int n, float time0, float time1)
+{
+    int axis = int(3 * random_double());
+
+    if (axis == 0)
+    {
+        qsort(l, n, sizeof(hitable*), box_x_compare);
+    }
+    else if (axis == 1)
+    {
+        qsort(l, n, sizeof(hitable*), box_y_compare);
+    }
+    else
+    {
+        qsort(l, n, sizeof(hitable*), box_z_compare);
+    }
+
+    if (n == 1)
+    {
+        left = right = l[0];
+    }
+    else if (n == 2)
+    {
+        left = l[0];
+        right = l[1];
+    }
+    else
+    {
+        left = new bvh_node(l, n / 2, time0, time1);
+        right = new bvh_node(l + n / 2, n - n / 2, time0, time1);
+    }
+
+    aabb box_left;
+    aabb box_right;
+    if (!left->bounding_box(time0, time1, box_left) || !right->bounding_box(time0, time1, box_right))
+    {
+        std::cerr << "no bounding box in bvh_node constructor\n";
+    }
+
+    box = surrounding_box(box_left, box_right);
+}
+
 #endif
